e16c54be86b091b2dddac4ad4c70ffee5c6a9d9e
Callan Stretton <callanstretton@Callans-MacBook-Pro.local>
2018-02-05 23:22:56 +1300

    removed commented out function

--- client/components/AddVideo.jsx
+++ client/components/AddVideo.jsx
@@ -77,27 +77,3 @@ class AddVideo extends React.Component {
 }
 
 export default AddVideo
-
-// const AddVideo = props => {
-//   return <div>
-
-//     <Header />
-//     <br />
-//     <br />
-//     <div className="something">
-//       <br />
-//       <h2 className="title subtitle is-2"><strong>Add a Scene Yourself!</strong></h2>
-//       <br />
-//       <p className="subtitle">
-//         Find a clip on <a href="https://www.youtube.com/" target="_blank"><img src='images/youtube.png' alt='YouTube' width='100'/></a> and paste it below.
-//       </p>
-//       <input type='text' className='new-vid-form'/>
-//       <br />
-//       <Link to="/">
-//         <button className="button is-large is-danger">Back to Home</button>
-//       </Link>
-//     </div>
-//   </div>
-// }
-
-// export default AddVideo
